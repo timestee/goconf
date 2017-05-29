45714204a89091651ae8665f354b75d4d949cf70
zsounder <wanghuidev@gmail.com>
2017-05-29 22:17:52 +0800

    fmt

--- examples/main.go
+++ examples/main.go
@@ -3,15 +3,15 @@ package main
 import "github.com/zsounder/goconf"
 
 type TestOptions struct {
-    goconf.AutoOptions
-    HTTPAddress string `default:"0.0.0.0:0000"`
-    Hosts []string `flag:"hosts" cfg:"hosts" default:"127.0.0.0,127.0.0.1"`
-    LogLevel int `default:"3"`
-    BoolVar bool `default:"false"`
+	goconf.AutoOptions
+	HTTPAddress string   `default:"0.0.0.0:0000"`
+	Hosts       []string `flag:"hosts" cfg:"hosts" default:"127.0.0.0,127.0.0.1"`
+	LogLevel    int      `default:"3"`
+	BoolVar     bool     `default:"false"`
 }
 
 func main() {
-   ops := &TestOptions{}
-   // conf_3 inherit from conf_1 and conf_2
-   goconf.ResolveAutoFlag(ops,"conf_3.toml")
+	ops := &TestOptions{}
+	// conf_3 inherit from conf_1 and conf_2
+	goconf.ResolveAutoFlag(ops, "conf_3.toml")
 }
